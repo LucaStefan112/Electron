e18d8348132364a3fbb98b6a1860231573b7c0fc
Luca-Stefan Tamas <lucastefantamas@gmail.com>
2021-12-03 01:42:38 +0200

    Updated Helper class

---
+++ include/Helper.h
@@ -0,0 +1,11 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+class Helper{
+public:
+    struct Vector_2D{double x = 0, y = 0;};
+
+    Vector_2D makeVector_2D(double x, double y);
+};
+
+#endif // HELPER_H
