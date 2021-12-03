ab9d97a4ead2f6d3ce81a51c4655df8b8fe72e65
Luca-Stefan Tamas <lucastefantamas@gmail.com>
2021-12-03 19:32:42 +0200

    Added header includer for all electronic components classes

---
+++ include/Electronic Components/Electronics.h
@@ -0,0 +1,8 @@
+#ifndef ELECTRONICS_H
+#define ELECTRONICS_H
+
+//Header for including all electronic components headers:
+#include "ElectronicComponent.h"
+#include "Diode.h"
+
+#endif // ELECTRONICS_H
