73f261316297eb6ec220c09fd8123bb29e2cd856
Luca-Stefan Tamas <lucastefantamas@gmail.com>
2021-12-06 17:32:10 +0200

    Updated Battery.cpp

--- src/Electronic Components/Battery.cpp
+++ src/Electronic Components/Battery.cpp
@@ -84,6 +84,7 @@ void Battery::Show(){
     double up_left_x = getPositionUpLeft().x, up_left_y = getPositionUpLeft().y;
     double center_x = getPositionCenter().x, center_y = getPositionCenter().y;
     double down_right_x = getPositionDownRight().x, down_right_y = getPositionDownRight().y;
+    double plusOff  = height / 8;
 
     line(up_left_x, center_y, center_x - height / 2, center_y);
     line(center_x - height / 2, up_left_y, center_x - height / 2, down_right_y);
@@ -91,6 +92,10 @@ void Battery::Show(){
     line(center_x - height / 2 + 2 * height / 3, up_left_y, center_x - height / 2 + 2 * height / 3, down_right_y);
     line(center_x + height / 2, center_y - height / 4, center_x + height / 2, center_y + height / 4);
     line(center_x + height / 2, center_y, down_right_x, center_y);
+
+    line(center_x - height / 1.5 - height / 4 - plusOff, center_y - height / 5 - plusOff, center_x - height / 1.5 - plusOff, center_y - height / 5 - plusOff);
+    line(center_x - height / 1.5 - height / 8 - plusOff, center_y - height / 5 - height / 8 - plusOff, center_x - height / 1.5 - height / 8 - plusOff, center_y - height / 5 + height / 8 - plusOff);
+    line(center_x + height / 1.1 - height / 4 + plusOff, center_y - height / 5 - plusOff, center_x + height / 1.1 + plusOff, center_y - height / 5 - plusOff);
 }
 
 //Passing trough a string all the data about the component:
