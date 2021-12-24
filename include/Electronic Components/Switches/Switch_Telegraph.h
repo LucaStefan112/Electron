6d82f993bef69edf8a7bd86db98dd03f69a21929
Luca-Stefan Tamas <lucastefantamas@gmail.com>
2021-12-24 13:41:57 +0200

    Rotate, Flip, Box, ConnectionPoint feature for Switches

--- include/Electronic Components/Switches/Switch_Telegraph.h
+++ include/Electronic Components/Switches/Switch_Telegraph.h
@@ -8,6 +8,9 @@ class Switch_Telegraph : public ElectronicComponent{
 public:
     Switch_Telegraph();
 
+    //Updating the position of each connection point:
+    void updateConnectionPointsPosition();
+
     //Drawing the component:
     void Show();
 };
--- src/Electronic Components/Switches/Switch_Open.cpp
+++ src/Electronic Components/Switches/Switch_Open.cpp
@@ -1,5 +1,7 @@
 #include "Electronic Components/Switches/Switch_Open.h"
 
+Helper Switch_Open_Helper;
+
 Switch_Open::Switch_Open(): ElectronicComponent(200, 50, "Open Switch", 2){}
 
 //Drawing the component:
@@ -7,13 +9,65 @@ void Switch_Open::Show(){
     double up_left_x = getPositionUpLeft().x, up_left_y = getPositionUpLeft().y;
     double center_x = getPositionCenter().x, center_y = getPositionCenter().y;
     double down_right_x = getPositionDownRight().x, down_right_y = getPositionDownRight().y;
-    const double sinAngle = 0.4472135955, cosAngle = 0.894427191;
 
-    circle(up_left_x + height / 10, center_y, height / 10);
-    line(up_left_x + height / 5, center_y, center_x - height / 2 - height / 10, center_y);
-    circle(center_x - height / 2, center_y, height / 10);
-    line(center_x - height / 2 + height / 10 * cosAngle, center_y - height / 10 * sinAngle, center_x + height / 2 - height / 9, up_left_y);
-    circle(center_x + height / 2, center_y, height / 10);
-    line(center_x + height / 2 + height / 10, center_y, down_right_x - height / 5, center_y);
-    circle(down_right_x - height / 10, center_y, height / 10);
+    if(flipped){
+        up_left_x = getPositionDownRight().x;
+        down_right_x = getPositionUpLeft().x;
+    }
+
+    double thisHeight = (down_right_x - up_left_x) / width_height_ratio;
+
+    Switch_Open_Helper.rotationalCircle(
+        up_left_x + thisHeight / 10, center_y, getPositionCenter(), height / 10,
+        rotateState);
+
+    Switch_Open_Helper.rotationalLine(
+        up_left_x + thisHeight / 5, center_y, center_x - thisHeight / 2 - thisHeight / 10, center_y, getPositionCenter(),
+        rotateState);
+
+    Switch_Open_Helper.rotationalCircle(
+        center_x - thisHeight / 2, center_y, getPositionCenter(), height / 10,
+        rotateState);
+
+    Switch_Open_Helper.rotationalLine(
+        center_x - thisHeight  * 0.41, center_y - height * 0.044721, center_x + thisHeight / 2 - thisHeight / 9, up_left_y, getPositionCenter(),
+        rotateState);
+
+    Switch_Open_Helper.rotationalCircle(
+        center_x + thisHeight / 2, center_y, getPositionCenter(), height / 10,
+        rotateState);
+
+    Switch_Open_Helper.rotationalLine(
+        center_x + thisHeight / 2 + thisHeight / 10, center_y, down_right_x - thisHeight / 5, center_y, getPositionCenter(),
+        rotateState);
+
+    Switch_Open_Helper.rotationalCircle(
+        down_right_x - thisHeight / 10, center_y, getPositionCenter(), height / 10,
+        rotateState);
+
+    showElements();
+}
+
+void Switch_Open::updateConnectionPointsPosition(){
+
+    double up_left_x = getPositionUpLeft().x, up_left_y = getPositionUpLeft().y;
+    double center_x = getPositionCenter().x, center_y = getPositionCenter().y;
+    double down_right_x = getPositionDownRight().x, down_right_y = getPositionDownRight().y;
+
+    if(flipped){
+        up_left_x = getPositionDownRight().x;
+        down_right_x = getPositionUpLeft().x;
+    }
+
+    double thisHeight = (down_right_x - up_left_x) / width_height_ratio;
+
+    connectionPoints[0].position = Switch_Open_Helper.rotatePointToReference(
+        Switch_Open_Helper.makeVector_2D(up_left_x + thisHeight / 10, center_y),
+        getPositionCenter(),
+        rotateState);
+
+    connectionPoints[1].position = Switch_Open_Helper.rotatePointToReference(
+        Switch_Open_Helper.makeVector_2D(down_right_x - thisHeight / 10, center_y),
+        getPositionCenter(),
+        rotateState);
 }
