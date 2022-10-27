41a022a33cc751a7d6a2aa4078768ab442cf2960
Tamas Luca-Stefan <lucastefantamas@gmail.com>
2022-10-27 14:14:44 +0300

    added graphics.h as local lib

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include <graphics.h>
+#include "./graphics.h"
 #include <winbgim.h>
 #include <windows.h>
-
 #include "Electronic Components/Electronics.h"
 #include "UI/UI.h"
 #include "Helper.h"
